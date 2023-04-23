e11a19570005da65ca697e80483547fa7e3ded6c
trucane <trufashionista2013@gmail.com>
2023-04-23 18:52:37 -0500

    Henry: added fontawesome package, icons for header and side nav, media queries, and responsiveness

--- src/Navigation/SideNav/SideNav.tsx
+++ src/Navigation/SideNav/SideNav.tsx
@@ -1,7 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faFileAlt, faTabletScreenButton } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
 import './sideNav.css';
 
 export const SideNav = () => {
     return(
-        <div className="side-nav-container">side nav</div>
+        <div className="side-nav-container">
+            {/* replace home icon with logo */}
+            <div className="side-nav-main-item">
+                <Link to="/"><FontAwesomeIcon icon={faHouse} /></Link>
+            </div>
+            <div className="side-nav-main-item">
+                <Link to="/"><FontAwesomeIcon icon={faFileAlt} /></Link>
+            </div>
+            <div className="side-nav-main-item">
+                <Link to="/"><FontAwesomeIcon icon={faTabletScreenButton} /></Link>
+            </div>
+        </div>
     )
 }
