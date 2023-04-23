f204f2d5ab7d52174f495fc0f2b92eb08dc32bee
trucane <trufashionista2013@gmail.com>
2023-04-23 16:48:31 -0500

    Henry: global resets, fonts, colors, more detail to page layout

---
+++ src/Navigation/MainHeader/MainHeader.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './main-header.css'
+
+
+
+export const MainHeaderContainer = () => {
+
+
+    return(
+        <div className="main-header-container">
+            <div className="main-header-links spacing">
+                <Link to="/">Home icon</Link>
+                <Link to="/about">About icon</Link>
+            </div>
+
+            <div className="main-header-user-container spacing">
+                <div className="language">language</div>
+                <div className="user-icon">home icon</div>
+                <a href="" type="btn" className="login">login /userName</a>
+            </div>
+        </div>
+    )
+}
