d84e4f7ad4568e266c4de6f5402007c3d328f235
trucane <trufashionista2013@gmail.com>
2023-04-23 19:15:40 -0500

    Henry: added back more icons

--- src/Navigation/MainHeader/MainHeader.tsx
+++ src/Navigation/MainHeader/MainHeader.tsx
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+// import { faBars, faHouse } from '@fortawesome/free-solid-svg-icons';
 import './main-header.css'
 
 
