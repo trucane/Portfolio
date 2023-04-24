5ea786ef466cc003af0a774920d6700856a11001
trucane <trufashionista2013@gmail.com>
2023-04-23 19:04:18 -0500

    Henry: removed eslint error

--- src/Navigation/SideNav/SideNav.tsx
+++ src/Navigation/SideNav/SideNav.tsx
@@ -1,12 +1,12 @@
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faHouse, faFileAlt, faTabletScreenButton } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './sideNav.css';
 
 export const SideNav = () => {
     return(
         <div className="side-nav-container">
-            {/* replace home icon with logo */}
+            {/* replace home icon with logo */ }
             {/* <div className="side-nav-main-item">
                 <Link to="/"><FontAwesomeIcon icon={faHouse} /></Link>
             </div> */}
